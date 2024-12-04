2d6b65687ad60498d8a754afcecb67c61083fe65
Uran <157412126+Uran4ik@users.noreply.github.com>
2024-12-04 20:20:12 +0300

    Work with JSON files

---
+++ WorkWithJson/DataBaseHandler.cs
@@ -0,0 +1,46 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresRedisDocker
+{
+    public class PostgresDB
+    {
+        string postgresConnectionString = "Host=localhost;Username=postgres;Port=5432;Password=password;Database=mydatabase";
+
+        public async Task CreateTable()
+        {
+            using (var connection = new NpgsqlConnection(postgresConnectionString))
+            {
+                await connection.OpenAsync();
+                var command = new NpgsqlCommand(@"CREATE TABLE IF NOT EXISTS Users 
+                    (Id SERIAL PRIMARY KEY,
+                    Name TEXT NOT NULL,
+                    Email TEXT NOT NULL,
+                    Age INT,
+                    IsActive BOOLEAN)", connection);
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task AddInDB(int id, string name, string email, int age, bool isActive)
+        {
+            using (var connection = new NpgsqlConnection(postgresConnectionString))
+            {
+                await connection.OpenAsync();
+                var command = new NpgsqlCommand(
+                    @"INSERT INTO Users (Id, Name, Email, Age, IsActive)
+                    VALUES (@id, @name, @email, @age, @isActive)", connection);
+                command.Parameters.AddWithValue("id", id);
+                command.Parameters.AddWithValue("name", name);
+                command.Parameters.AddWithValue("email", email);
+                command.Parameters.AddWithValue("age", age);
+                command.Parameters.AddWithValue("isActive", isActive);
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
---
+++ WorkWithJson/RedisHandler.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresRedisDocker
+{
+    public class RedisHandler
+    {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase bd;
+
+            public RedisHandler()
+                => bd = redis.GetDatabase();
+            public async Task SetData(string index, string value)
+                => await bd.StringSetAsync(index, value);
+            
+            public async Task<string> GetData(string index)
+                => await bd.StringGetAsync(index);
+        }
+    }
