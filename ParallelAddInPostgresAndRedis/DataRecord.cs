d422a9126b98c5e0e882a06b2a4ef12928fee67d
Uran <157412126+Uran4ik@users.noreply.github.com>
2024-12-11 19:50:48 +0300

    Parallel

---
+++ ParallelAddInPostgresAndRedis/DataRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresRedisDocker
+{
+    public class UserClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ParallelAddInPostgresAndRedis/Program.cs
@@ -0,0 +1,76 @@
+﻿using Npgsql;
+using StackExchange;
+using System.Text;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.IO;
+using StackExchange.Redis;
+
+namespace PostgresRedisDocker
+{
+    public interface IReadJsonStrat
+    {
+        Task<UserClass[]> ReadJson(string path);
+    }
+    public class ReadJsonStrat : IReadJsonStrat
+    {
+        public async Task<UserClass[]> ReadJson(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string content = await reader.ReadToEndAsync();
+
+                UserClass[] users = JsonConvert.DeserializeObject<UserClass[]>(content);
+                return users;
+            }
+        }
+    }
+
+    public class JSONDelegate
+    {
+        public event Func<UserClass[], Task> DataRead;
+
+        public async Task Read(string path, IReadJsonStrat strat)
+        {
+            UserClass[] users = await strat.ReadJson(path);
+            if (DataRead != null)
+                await DataRead(users);
+        }
+    }
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string jsonPath = @"C:\Users\Нурмухаммед\Downloads\Telegram Desktop\users2.json";
+
+            var postgres = new PostgresDB();
+            var redis = new RedisHandler();
+
+            await postgres.CreateTable();
+
+            try
+            {
+                var reader = new JSONDelegate();
+                reader.DataRead += async (users) =>
+                {
+                    await Parallel.ForEachAsync(users, async (user, cancellationToken) =>
+                    {
+                        await postgres.AddInDB(user.Id, user.Name, user.Email, user.Age, user.IsActive);
+                        await redis.SetData("Last", JsonConvert.SerializeObject(user));
+                    });
+                    Console.WriteLine("\nЗапись добавлена в Постгрес \nПоследняя запись в Редис обновлена");
+                };
+
+                IReadJsonStrat strat = new ReadJsonStrat();
+                string last = await redis.GetData("Last");
+                await reader.Read(jsonPath, strat);
+
+                Console.WriteLine($"Последняя запись в Redis: {last}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.ToString());
+            }
+        }
+    }
+}
